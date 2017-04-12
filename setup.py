0d42c8df92f39b455950e8ed985795c25850a96b
Mike 'Fuzzy' Partin <fuzzy@fumanchu.org>
2017-04-12 05:08:06 -0700

    updated setup procedures with some system checking and informational output

--- setup.py
+++ setup.py
@@ -1,7 +1,55 @@
 #!/usr/bin/env python2
 
+# Stdlib
+import os
+
 from distutils.core import setup
 
+
+# Internal
+from abused.scale import *
+
+
+# Output flags
+info  = Scale('>>>').green()
+warn  = Scale('!!!').bold().yellow()
+error = Scale('!!').bold().red()
+    
+# Determine if we have sudo, which will be required for non-root operations
+# Absense is not fatal, but it's use is encouraged.
+have_sudo = False
+for p in os.getenv('PATH').split(':'):
+    try:
+        if 'sudo' in os.listdir(p):
+            have_sudo = True
+            break
+    except OSError:
+        pass
+if not have_sudo:
+    print("%s You do not have sudo installed." % warn)
+    print("%s It's presence is not mandatory, but is encouraged." % warn)
+else:
+    print('%s Sudo installed: %s' % (info, Scale('True').bold().green()))
+
+# Determine if we have pyyaml installed
+try:
+    import yaml
+    try:
+        from yaml import CLoader as Loader, CDumper as Dumper
+    except ImportError:
+        from yaml import Loader, Dumper
+    print('%s PyYAML installed: %s' % (info, Scale('True').bold().green()))
+    #cfg = yaml.load(open('./abused.cfg'), Loader=Loader)
+except ImportError:
+    print('%s You do not have PyYAML installed.' % error)
+    print('%s You can install it with the following command:' % error)
+    print('%s pip install -r requirements.txt' % error)
+    #system('%spip install -r requirements.txt')
+    #import yaml
+    sys.exit(1)
+
+print('%s Installing...\n' % info)
+# Do the big final dance 
 setup(
     name='abused',
     version='0.1.3',
@@ -12,3 +60,6 @@ setup(
     packages=['abused', 'abused.editor'],
     scripts=['bin/abused', 'bin/abused_usefix'],
 )
+
+print('\n%s You should copy the abused.cfg to either /etc/abused/abused.cfg,' % info)
+print('%s ${HOME}/.abused.cfg, or always run abused from this diretory.\n' % info)
