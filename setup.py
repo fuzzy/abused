149d3b4f0accc2af4c8e43a0ade57e89c283a65e
Mike 'Fuzzy' Partin <fuzzy@thwap.org>
2021-03-22 04:40:59 -0700

    whitespace fixes and pre-commit configs

--- setup.py
+++ setup.py
@@ -11,16 +11,16 @@ from abused.scale import *
 
 
 # Output flags
-info  = Scale('>>>').green()
-warn  = Scale('!!!').bold().yellow()
-error = Scale('!!').bold().red()
-    
+info = Scale(">>>").green()
+warn = Scale("!!!").bold().yellow()
+error = Scale("!!").bold().red()
+
 # Determine if we have sudo, which will be required for non-root operations
 # Absense is not fatal, but it's use is encouraged.
 have_sudo = False
-for p in os.getenv('PATH').split(':'):
+for p in os.getenv("PATH").split(":"):
     try:
-        if 'sudo' in os.listdir(p):
+        if "sudo" in os.listdir(p):
             have_sudo = True
             break
     except OSError:
@@ -29,47 +29,52 @@ if not have_sudo:
     print("%s You do not have sudo installed." % warn)
     print("%s It's presence is not mandatory, but is encouraged." % warn)
 else:
-    print('%s Sudo installed: %s' % (info, Scale('True').bold().green()))
+    print("%s Sudo installed: %s" % (info, Scale("True").bold().green()))
 
 # Determine if we have pyyaml installed
 try:
     import yaml
+
     try:
         from yaml import CLoader as Loader, CDumper as Dumper
     except ImportError:
         from yaml import Loader, Dumper
-    print('%s PyYAML installed: %s' % (info, Scale('True').bold().green()))
-    #cfg = yaml.load(open('./abused.cfg'), Loader=Loader)
+    print("%s PyYAML installed: %s" % (info, Scale("True").bold().green()))
+    # cfg = yaml.load(open('./abused.cfg'), Loader=Loader)
 except ImportError:
-    print('%s You do not have PyYAML installed.' % error)
-    print('%s You can install it with the following command:' % error)
-    print('%s pip install -r requirements.txt' % error)
-    #system('%spip install -r requirements.txt')
-    #import yaml
+    print("%s You do not have PyYAML installed." % error)
+    print("%s You can install it with the following command:" % error)
+    print("%s pip install -r requirements.txt" % error)
+    # system('%spip install -r requirements.txt')
+    # import yaml
     sys.exit(1)
 
-print('%s Installing...\n' % info)
-# Do the big final dance 
+print("%s Installing...\n" % info)
+# Do the big final dance
 setup(
-    name='abused',
-    version='0.3.6',
-    description='A Basic USE eDitor is an inline use flag editor for Gentoo Linux.',
+    name="abused",
+    version="0.3.6",
+    description="A Basic USE eDitor is an inline use flag editor for Gentoo Linux.",
     author="Mike 'Fuzzy' Partin",
-    author_email='fuzzy@fumanchu.org',
-    url='https://git.thwap.org/fuzzy/abused',
-    packages=['abused', 'abused.editor'],
-    scripts=['bin/abused', 'bin/abused_usefix'],
+    author_email="fuzzy@fumanchu.org",
+    url="https://git.thwap.org/fuzzy/abused",
+    packages=["abused", "abused.editor"],
+    scripts=["bin/abused", "bin/abused_usefix"],
 )
 
-existing_config=False
-for i in ('/etc/abused/abused.cfg', '%s/.abusedrc' % os.getenv('HOME')):
+existing_config = False
+for i in ("/etc/abused/abused.cfg", "%s/.abusedrc" % os.getenv("HOME")):
     if os.path.exists(i):
-        existing_config=True
-        
+        existing_config = True
+
 if not existing_config:
-    print('\n%s You should copy the abused.cfg to either /etc/abused/abused.cfg,' % info)
-    print('%s ${HOME}/.abusedrc, or always run abused from this diretory.\n' % info)
+    print(
+        "\n%s You should copy the abused.cfg to either /etc/abused/abused.cfg," % info
+    )
+    print("%s ${HOME}/.abusedrc, or always run abused from this diretory.\n" % info)
 else:
-    print('\n%s Pre-existing installation detected. Please review the abused.cfg,' % info)
-    print('%s in the source distribution and see if any changes are needed in' % info)
-    print('%s your configuration files.' % info)
+    print(
+        "\n%s Pre-existing installation detected. Please review the abused.cfg," % info
+    )
+    print("%s in the source distribution and see if any changes are needed in" % info)
+    print("%s your configuration files." % info)
