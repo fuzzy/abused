60372eb2580c71ce69b958fe7b264769a12995d9
Mike 'Fuzzy' Partin <fuzzy@thwap.org>
2021-03-24 11:20:30 -0700

    removed PegParser class

--- abused/emerge.py
+++ abused/emerge.py
@@ -14,26 +14,6 @@ from abused.config import *
 from abused.package import *
 
 
-class PegParser:
-
-    _callbacks: dict
-    _expressions: dict
-
-    def add_trigger(self, patt: str, cbak: Callable) -> bool:
-        return False
-
-    def del_trigger(self, id: str) -> bool:
-        return False
-
-    def process(self, infp: TextIO) -> None:
-        pass
-
-
-class EmergeParser:
-    def __init__(self):
-        pass
-
-
 class Emerge:
     def __init__(self):
         self._cfg = read_config()
