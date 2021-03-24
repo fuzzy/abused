624dfb0a7dd90fc7ecdf7a4c1f4f8e2eac38d469
Mike 'Fuzzy' Partin <fuzzy@thwap.org>
2021-03-24 11:35:18 -0700

    added some meat to the methods, process is still in flux

--- abused/outproc.py
+++ abused/outproc.py
@@ -14,14 +14,29 @@ class PegParser:
     _callbacks: dict
     _expressions: dict
 
-    def add_trigger(self, patt: str, cbak: Callable) -> bool:
-        return False
+    def add_trigger(self, id: str, patt: str, cbak: Callable) -> bool:
+        try:
+            self._expressions[id] = re.compile(patt)
+            self._callbacks[id] = cbak
+            return True
+        except:
+            return False
 
     def del_trigger(self, id: str) -> bool:
-        return False
+        try:
+            self._expressions.pop(id)
+            self._callbacks.pop(id)
+            return True
+        except KeyError:
+            return False
 
     def process(self, infp: TextIO) -> None:
-        pass
+        try:
+            buff = infp.readline()
+            while buff:
+                buff = infp.readline()
+        except AttributeError:
+            return
 
 
 class OutProc:
